65938fa127dd9eb1f6e03dc6cf46144e9d677267
Shelly Tang <shellytang@gmail.com>
2017-01-03 00:35:56 -0800

    added wireframes and setup for html and css files

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function myProjects(projectName, dateFinished, description, link) {
+  this.projectName = projectName;
+  this.dateFinished = dateFinished;
+  this.description = description;
+  this.link = link;
+}
