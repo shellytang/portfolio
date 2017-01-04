12e5e0165d04dafd947ab1bcc75774cbd4f9006b
Shelly Tang <shellytang@gmail.com>
2017-01-03 17:39:49 -0800

    updated HTML and started js files for constructor

---
+++ scripts/projectArticles.js
@@ -0,0 +1,34 @@
+var projectArticles = [
+  {
+    title: 'Website Redesign',
+    client: 'Corporate',
+    author: 'Shelly Tang and Zach Puckett',
+    authorURL: 'github address',
+    publishedOn: '2016-11-15',
+    body: '<p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.<p>'
+  },
+  {
+    title: 'Small Business Website',
+    client: 'Meow Bakery',
+    author: 'Shelly Tang and Carlee McConnell',
+    authorURL: 'github address',
+    publishedOn: '2016-12-01',
+    body: 'Toffee marzipan soufflé croissant jelly dessert biscuit chocolate bar gummi bears. Donut pudding gummi bears sweet oat cake jelly-o. Carrot cake icing chocolate cake jujubes biscuit. Topping brownie sesame snaps ice cream carrot cake ice cream cake cookie. Dessert pastry cotton candy jelly beans soufflé sesame snaps muffin apple pie chupa chups. Jelly-o gummies danish bonbon tootsie roll donut marzipan cotton candy cake. Sesame snaps wafer lemon drops lollipop candy canes topping cake pastry. Gummies ice cream cupcake gummi bears. Chupa chups candy tiramisu ice cream. Chocolate sugar plum jelly-o toffee candy canes tootsie roll sugar plum.'
+  },
+  {
+    title: 'Produce Order Form',
+    client: 'Happy Farm',
+    author: 'Shelly Tang',
+    authorURL: 'github address',
+    publishedOn: '2015-12-10',
+    body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.'
+  },
+  {
+    title: 'E-Commerce Site',
+    client: 'Little Boutique',
+    author: 'Shelly Tang',
+    authorURL: 'github address',
+    publishedOn: '2015-12-30',
+    body: 'Pastels Céline leggings leather shorts Saint Laurent beanie street style Fjallraven. Motif skirt strong eyebrows Choupette printed jacket grunge white chambray shirt denim. Colette chunky sole loafers trouser surf pop Prada backpack washed out statement Bag Noun silver collar. Rope necklace oversized A.P.C. rose gold knitwear capsule Raf Simons.'
+  },
+];
