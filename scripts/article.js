83b4b3fdcc297442c6adb8b6777371be279bd00d
Shelly Tang <shellytang@gmail.com>
2017-01-05 07:51:31 -0800

    cleaned up code in projectArticles.js file and removed date tracker and sort

--- scripts/article.js
+++ scripts/article.js
@@ -3,7 +3,6 @@
 var articles = [];
 
 function Article (options) {
-  this.author = options.author;
   this.body = options.body;
   this.title = options.title;
   this.client = options.client;
@@ -11,19 +10,13 @@ function Article (options) {
 }
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  $newArticle.find('address a').attr('href', this.client);
   $newArticle.find('p').text(this.completed);
   $newArticle.find('.article-body').html(this.body);
   $newArticle.find('h1').text(this.title);
   $newArticle.find('h2').text(this.client);
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.removeClass('template');
   return $newArticle;
 };
-blogArticles.sort(function(currentObject, nextObject) {
-  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
-});
 blogArticles.forEach(function(ele) {
   articles.push(new Article(ele));
 });
