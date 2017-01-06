4483fe6143a9e7805265f8b1799c1225afe5d909
Shelly Tang <shellytang@gmail.com>
2017-01-05 22:20:10 -0800

    added comments to html file for reference

--- scripts/article.js
+++ scripts/article.js
@@ -1,7 +1,6 @@
 'use strict';
-//code taken from day 2 and refactored slightly
-var articles = [];
 
+var articles = [];
 function Article (options) {
   this.body = options.body;
   this.title = options.title;
@@ -10,14 +9,6 @@ function Article (options) {
   this.image = options.image;
 }
 Article.prototype.toHtml = function() {
-  // var $newArticle = $('article.template').clone();
-  // $newArticle.find('p').text(this.completed);
-  // $newArticle.find('.article-body').html(this.body);
-  // $newArticle.find('h1').text(this.title);
-  // $newArticle.find('h2').text(this.client);
-  // $newArticle.find('.image').html(this.image);
-  // $newArticle.removeClass('template');
-  // return $newArticle;
   var source = $('#project-template').html();
   var templateRender = Handlebars.compile(source);
   return templateRender(this);
