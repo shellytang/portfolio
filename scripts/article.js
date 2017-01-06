5b3cd4fcd5b35d0ff8750963c1fd5b10407ab649
Shelly Tang <shellytang@gmail.com>
2017-01-05 22:09:34 -0800

    refactored html and js file to use handlebars

--- scripts/article.js
+++ scripts/article.js
@@ -7,15 +7,20 @@ function Article (options) {
   this.title = options.title;
   this.client = options.client;
   this.completed = options.completed;
+  this.image = options.image;
 }
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.find('p').text(this.completed);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('h1').text(this.title);
-  $newArticle.find('h2').text(this.client);
-  $newArticle.removeClass('template');
-  return $newArticle;
+  // var $newArticle = $('article.template').clone();
+  // $newArticle.find('p').text(this.completed);
+  // $newArticle.find('.article-body').html(this.body);
+  // $newArticle.find('h1').text(this.title);
+  // $newArticle.find('h2').text(this.client);
+  // $newArticle.find('.image').html(this.image);
+  // $newArticle.removeClass('template');
+  // return $newArticle;
+  var source = $('#project-template').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
 };
 blogArticles.forEach(function(ele) {
   articles.push(new Article(ele));
--- scripts/projectArticles.js
+++ scripts/projectArticles.js
@@ -2,24 +2,28 @@
 
 var blogArticles = [
   {
+    image: '<img src="images/consulting.jpg" class="projectimage" id="consulting"></img>',
     title: 'Website Redesign',
     client: 'The Consulting Group',
     completed: 'June 2016',
     body: '<p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.<p>'
   },
   {
+    image: '<img src="images/bakery.jpg" class="projectimage" id="bakery"></img>',
     title: 'Small Business Website',
     client: 'Meow Bakery',
     completed: 'August 2016',
     body: 'Toffee marzipan soufflé croissant jelly dessert biscuit chocolate bar gummi bears. Donut pudding gummi bears sweet oat cake jelly-o. Carrot cake icing chocolate cake jujubes biscuit. Topping brownie sesame snaps ice cream carrot cake ice cream cake cookie. Dessert pastry cotton candy jelly beans soufflé sesame snaps muffin apple pie chupa chups. Jelly-o gummies danish bonbon tootsie roll donut marzipan cotton candy cake. Sesame snaps wafer lemon drops lollipop candy canes topping cake pastry. Gummies ice cream cupcake gummi bears. Chupa chups candy tiramisu ice cream. Chocolate sugar plum jelly-o toffee candy canes tootsie roll sugar plum.'
   },
   {
+    image: '<img src="images/farm.jpg" class="projectimage" id="farm"></img>',
     title: 'Produce Order Form App',
     client: 'Happy Farm',
     completed: 'Oct 2016',
     body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.'
   },
   {
+    image: '<img src="images/boutique.jpg" class="projectimage" id="boutique"></img>',
     title: 'E-Commerce Site',
     client: 'Little Boutique',
     completed: 'Nov 2016',
