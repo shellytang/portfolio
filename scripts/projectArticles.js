df0913ce5d642078d003659d7dbbe2a7c936ffe4
Shelly Tang <shellytang@gmail.com>
2017-01-07 09:34:02 -0800

    updated about me page

--- scripts/projectArticles.js
+++ scripts/projectArticles.js
@@ -13,20 +13,20 @@ var blogArticles = [
     title: 'Small Business Website',
     client: 'Meow Bakery',
     completed: 'August 2016',
-    body: 'Toffee marzipan soufflé croissant jelly dessert biscuit chocolate bar gummi bears. Donut pudding gummi bears sweet oat cake jelly-o. Carrot cake icing chocolate cake jujubes biscuit. Topping brownie sesame snaps ice cream carrot cake ice cream cake cookie. Dessert pastry cotton candy jelly beans soufflé sesame snaps muffin apple pie chupa chups. Jelly-o gummies danish bonbon tootsie roll donut marzipan cotton candy cake. Sesame snaps wafer lemon drops lollipop candy canes topping cake pastry. Gummies ice cream cupcake gummi bears. Chupa chups candy tiramisu ice cream. Chocolate sugar plum jelly-o toffee candy canes tootsie roll sugar plum.'
+    body: '<p>Toffee marzipan soufflé croissant jelly dessert biscuit chocolate bar gummi bears. Donut pudding gummi bears sweet oat cake jelly-o. Carrot cake icing chocolate cake jujubes biscuit. Topping brownie sesame snaps ice cream carrot cake ice cream cake cookie. Dessert pastry cotton candy jelly beans soufflé sesame snaps muffin apple pie chupa chups. Jelly-o gummies danish bonbon tootsie roll donut marzipan cotton candy cake. Sesame snaps wafer lemon drops lollipop candy canes topping cake pastry. Gummies ice cream cupcake gummi bears. Chupa chups candy tiramisu ice cream. Chocolate sugar plum jelly-o toffee candy canes tootsie roll sugar plum.</p>'
   },
   {
     image: '<img src="images/farm.jpg" class="projectimage" id="farm"></img>',
     title: 'Produce Order Form App',
     client: 'Happy Farm',
     completed: 'Oct 2016',
-    body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.'
+    body: '<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>'
   },
   {
     image: '<img src="images/boutique.jpg" class="projectimage" id="boutique"></img>',
     title: 'E-Commerce Site',
     client: 'Little Boutique',
     completed: 'Nov 2016',
-    body: 'Pastels Céline leggings leather shorts Saint Laurent beanie street style Fjallraven. Motif skirt strong eyebrows Choupette printed jacket grunge white chambray shirt denim. Colette chunky sole loafers trouser surf pop Prada backpack washed out statement Bag Noun silver collar. Rope necklace oversized A.P.C. rose gold knitwear capsule Raf Simons.'
+    body: '<p>Pastels Céline leggings leather shorts Saint Laurent beanie street style Fjallraven. Motif skirt strong eyebrows Choupette printed jacket grunge white chambray shirt denim. Colette chunky sole loafers trouser surf pop Prada backpack washed out statement Bag Noun silver collar. Rope necklace oversized A.P.C. rose gold knitwear capsule Raf Simons.</p>'
   },
 ];
