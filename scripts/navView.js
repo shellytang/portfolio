8eb69edd480dbea864e985485aff024fb6105cf3
Shelly Tang <shellytang@gmail.com>
2017-01-07 21:03:50 -0800

    added in hide article and hover functionality

--- scripts/navView.js
+++ scripts/navView.js
@@ -1,5 +1,6 @@
 'use strict';
 function navView () {
+  toggleArticleBody();
   $('.main-nav').on('click', '.tab', function(){
     $('.tab-content').hide();
     $('#'+$(this).attr('data-content')).fadeIn();
@@ -7,3 +8,11 @@ function navView () {
   $('.main-nav .tab:first').click();
 }
 navView();
+
+
+function toggleArticleBody () {
+  $('.article-body').hide();
+  $('.image').on('click', function (){
+    $(this).siblings('.article-body').toggle();
+  });
+}
