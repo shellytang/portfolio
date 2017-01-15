7706c133ac0ece661344d4478d95263e6cbbb415
Shelly Tang <shellytang@gmail.com>
2017-01-14 16:43:40 -0800

    wrapped modules in IFFE

--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var projectView = {};
+(function(module){
 
-projectView.handleMainNav = function () {
-  projectView.toggleArticleBody();
-  $('.main-nav').on('click', '.tab', function(){
-    $('.tab-content').hide();
-    $('#'+$(this).attr('data-content')).fadeIn();
-  });
-  $('.main-nav .tab:first').click();
-}
-projectView.toggleArticleBody = function () {
-  $('.article-body').hide();
-  $('#articles h2, #articles h3').hide();
-  $('.image').on('click', function (){
-    $(this).siblings('h2, h3, .article-body').toggle();
-  });
-}
+  var projectView = {};
 
-projectView.initIndexPage = function () {
-  Article.all.forEach(function(article) {
-    $('#articles').append(article.toHtml());
-  });
-  projectView.handleMainNav();
-}
+  projectView.handleMainNav = function () {
+    projectView.toggleArticleBody();
+    $('.main-nav').on('click', '.tab', function(){
+      $('.tab-content').hide();
+      $('#'+$(this).attr('data-content')).fadeIn();
+    });
+    $('.main-nav .tab:first').click();
+  }
+  projectView.toggleArticleBody = function () {
+    $('.article-body').hide();
+    $('#articles h2, #articles h3').hide();
+    $('.image').on('click', function (){
+      $(this).siblings('h2, h3, .article-body').toggle();
+    });
+  }
+
+  projectView.initIndexPage = function () {
+    Article.all.forEach(function(article) {
+      $('#articles').append(article.toHtml());
+    });
+    projectView.handleMainNav();
+  }
 
 // ++++ desktop view: hide images and show description of clicked on image. will need to uncomment div .show and in css desktopView if testing. this also broke the reload of portfolio page ++++++
 // function toggleArticleBody () {
@@ -34,3 +36,5 @@ projectView.initIndexPage = function () {
 //     $(this).siblings().addClass('desktopView').children().fadeIn();
 //   });
 // }
+  module.projectView = projectView;
+})(window);
