21b77c6c0976a78426a111648d177078e795ce8a
Shelly Tang <shellytang@gmail.com>
2017-01-18 21:21:00 -0800

    got github stats to render

--- public/scripts/views/repoView.js
+++ public/scripts/views/repoView.js
@@ -2,15 +2,10 @@
 
 (function(module) {
   const repoView = {};
-  // const ui = function() {
-  //   let $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
-  //   $about.find('ul').empty();
-  //   $about.show().siblings().hide();
-  // };
+
   repoView.index = function() {
-    // ui();
     var render = Handlebars.compile($('#repo-template').text());
-    $('#githubRepo ul').append(repos.with('name').map(render));
+    $('#githubRepo').append(repos.with('name').map(render));
   };
   module.repoView = repoView;
 })(window);
