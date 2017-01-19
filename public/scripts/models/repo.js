8d5be40c4e3c726a682d8af1b66b581c46815911
Shelly Tang <shellytang@gmail.com>
2017-01-18 21:22:02 -0800

    got github data to render

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -1,23 +1,24 @@
 'use strict';
 
-(function(module){
-const repos = {};
+(function(module) {
+  const repos = {};
 
-repos.all = [];
+  repos.all = [];
 
-repos.requestRepos = function(callback){
-  $.ajax({
-    url: 'https:api.github.com/users/shellytang/repos?type=owner',
-    method: 'GET',
-    headers: {
-      Authorization: `token ${githubToken}`
-    }
-  })
-  .then(data => {
-    repos.all = data;
-    callback();
-  })
-};
-  repos.with = attr => repos.all.filter(repo => repo[attr]);
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https:api.github.com/users/shellytang/repos?type=owner',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}`
+        // Authorization: 'token ' + githubToken
+      }
+    })
+    .then(data => {
+      repos.all = data;
+      callback();
+    })
+  };
+  repos.with = fork => repos.all.filter(repo => repo !== [fork]);
   module.repos = repos;
 })(window);
