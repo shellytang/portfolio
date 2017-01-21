233fcae90bfb2c6cd98a08d42ddea8a48cb79d3c
Shelly Tang <shellytang@gmail.com>
2017-01-20 18:57:40 -0800

    added proxy and updated request

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -5,20 +5,27 @@
 
   repos.all = [];
 
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     url: 'https:api.github.com/users/shellytang/repos?type=owner',
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `token ${githubToken}`
+  //       // Authorization: 'token ' + githubToken
+  //     }
+  //   })
+  //   .then(data => {
+  //     repos.all = data;
+  //     callback();
+  //   })
+  // };
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https:api.github.com/users/shellytang/repos?type=owner',
-      method: 'GET',
-      headers: {
-        Authorization: `token ${githubToken}`
-        // Authorization: 'token ' + githubToken
-      }
-    })
-    .then(data => {
-      repos.all = data;
-      callback();
-    })
+  // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+    $.get('/github/user/repos?type=owner') //remove .com!!
+    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(callback);
   };
+// repos.with = attr => repos.all.filter(repo => repo[attr]);
   repos.with = fork => repos.all.filter(repo => repo !== [fork]);
   module.repos = repos;
 })(window);
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 'use strict';
 
 const express = require('express');
+const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.static('./public'));
 app.get('*', function(request, response) {
   response.sendFile('index.html', {root: './public'})
 });
+
+// REVIEW: This is a new route that will utilize our middle man proxy.
+app.get('/github/*', proxyGitHub);
+
+// REVIEW: This is a new proxy method which acts as a 'middle man' (middleware) for our request.
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response); //<--requestProxy is immediately invoked and returns a function
+}
+
 app.listen(PORT, function(){
   console.log('Server is up and running on port 4000 and can be accessed at local host 4000 in your browser');
 })
