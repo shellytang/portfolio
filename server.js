04b3127974669fa7f27c6b8946e42b4433e40259
Shelly Tang <shellytang@gmail.com>
2017-01-20 23:30:43 -0800

    updated route

--- server.js
+++ server.js
@@ -5,19 +5,18 @@ const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.static('./public'));
-app.get('*', function(request, response) {
+app.get('/', function(request, response) {
   response.sendFile('index.html', {root: './public'})
 });
 
 app.get('/github/*', proxyGitHub);
-//
-// // REVIEW: This is a new proxy method which acts as a 'middle man' (middleware) for our request.
+
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
     headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
-  }))(request, response); //<--requestProxy is immediately invoked and returns a function
+  }))(request, response);
 }
 
 app.listen(PORT, function(){
